8e7de9deaa4f118ed051d2e2281012c3f37f577a
Efe-code1 <efezzy321@gmail.com>
2024-01-27 06:36:22 +0100

    lined out

--- To-Do List.js
+++ To-Do List.js
@@ -1,14 +1,18 @@
+
+
 var todoList = document.getElementById('todoList');
 var additems = document.getElementById('addTodo');
 
 additems.onclick = function(){
-    var todo_obj = document.getElementById("todo").value;
+    var todo = document.getElementById("todo").value;
     
-    if (todo_obj !== ""){
+    if (todo !== ""){
         
         var li = document.createElement('li');
-        li.innerHTML = todo_obj;
+        li.innerHTML = todo;
         todoList.appendChild(li);
         document.getElementById("todo").value = "";
     }
+
 }
+
